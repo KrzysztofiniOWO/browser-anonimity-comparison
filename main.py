c944782892eb9b1bd3add01701bcf0b282dd3281
krzysztof <molakrzysztof@gmail.com>
2026-02-07 20:23:39 +0100

    Add tests for browserleaks DNS section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scripts import (
     get_amiunique_data,
+    get_browserleaks_DNS,
     get_browserleaks_IP_data,
     get_browserleaks_JS_data,
     get_browserscan_data,
@@ -8,19 +9,19 @@ from scripts import (
     get_ipinfo_data,
     get_ipleak_data,
     get_pixelscan_data
-
 )
 
 def main():
-    get_amiunique_data.main()
-    get_browserleaks_IP_data.main()
-    get_browserleaks_JS_data.main()
-    get_browserscan_data.main()
-    get_creepjs_data.main()
-    get_deviceinfo_data.main()
-    get_ipinfo_data.main()
-    get_ipleak_data.main()
-    get_pixelscan_data.main()
+    # get_amiunique_data.main()
+    get_browserleaks_DNS.main()
+    # get_browserleaks_IP_data.main()
+    # get_browserleaks_JS_data.main()
+    # get_browserscan_data.main()
+    # get_creepjs_data.main()
+    # get_deviceinfo_data.main()
+    # get_ipinfo_data.main()
+    # get_ipleak_data.main()
+    # get_pixelscan_data.main()
 
 if __name__ == "__main__":
     main()
