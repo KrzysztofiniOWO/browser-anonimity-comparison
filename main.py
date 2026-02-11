d77feff86fa686fb6fb89929722d65fb43dd0452
krzysztof <molakrzysztof@gmail.com>
2026-02-11 20:41:09 +0100

    Analysis system part 1

--- main.py
+++ main.py
@@ -13,19 +13,30 @@ from scripts import (
     get_pixelscan_data
 )
 
+from analyse import (
+    analysis
+)
+
 def main():
+    # Funtions to scrap data for every webpage
+
     # get_amiunique_data.main()
-    #get_browserleaks_DNS.main()
+    # get_browserleaks_DNS.main()
     # get_browserleaks_IP_data.main()
     # get_browserleaks_JS_data.main()
     # get_browserscan_data.main()
     # get_creepjs_data.main()
     # get_deviceinfo_data.main()
-    get_fingerprint_scan_data.main()
+    # get_fingerprint_scan_data.main()
     # get_fingerprintJS_data.main()
     # get_ipinfo_data.main()
     # get_ipleak_data.main()
     # get_pixelscan_data.main()
+
+    #Functions to analyze data for every webpage
+
+    analysis.main("amiunique")
+    analysis.main("browserleaks_ip")
 
 if __name__ == "__main__":
     main()
